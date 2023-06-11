26e6d2f87f9a6b6fcbb0f8930883c3622e6125da
Ilya Yarusov <originalyia@gmail.com>
2023-06-11 12:33:30 +0800

    code recycling

--- crypto_portfolio.py
+++ crypto_portfolio.py
@@ -1,62 +1,53 @@
 #!/usr/bin/python3
-share_assets = {
-        'BTC': 0.3,
-        'BNB': 0.10,
-        'ETH': 0.25,
-        'DOT': 0.05,
-        'NEAR': 0.05,
-        'ADA': 0.05,
-        'ATOM': 0.05,
-        'MATIC': 0.15
-}
-
-cash = 100
-number_averages = 1
+import sys
+import math
 
 
-def checking_proportion_assets(assets):
-    sum = 0
-    for share in share_assets.values():
-        sum = sum + share
-    return sum
+class Portfolio():
+    def __init__(self):
+        self.asset_proportions = {
+                'BTC': 0.3,
+                'BNB': 0.10,
+                'ETH': 0.25,
+                'DOT': 0.05,
+                'NEAR': 0.05,
+                'ADA': 0.05,
+                'ATOM': 0.05,
+                'MATIC': 0.15
+        }
+        self.cash = 120
+        self.number_averages = 1
 
 
-def creating_list_purchase_price_montly():
-
-    list_purchase_price_monthly = {}
-
-    for asset_name, fraction in share_assets.items():
-        asset_purchase_price = fraction * cash
-        list_purchase_price_monthly[asset_name] = asset_purchase_price
-
-    return list_purchase_price_monthly
-
-
-def creating_list_purchase_price_once(list_purchase_price_monthly):
-
-    list_purchase_price_once = {}
-
-    for asset_name, price in list_purchase_price_monthly.items():
-        asset_purchase_price = price / number_averages
-        list_purchase_price_once[asset_name] = asset_purchase_price
-
-    return list_purchase_price_once
+def check_proportions_assets(asset_proportions: dict) -> bool:
+    check_result = False
+    amount_assets = 0
+    for value in asset_proportions.values():
+        amount_assets += value
+    print(amount_assets)
+    if math.isclose(amount_assets, 1, rel_tol=1e-7):
+        check_result = True
+    return check_result
 
 
-def final_calculations():
-
-    sum_assets = checking_proportion_assets(share_assets)
-
-    print(sum_assets)
-    list_purchase_price_monthly = creating_list_purchase_price_montly()
-    list_purchase_price_once = creating_list_purchase_price_once(list_purchase_price_monthly)
-    print(list_purchase_price_monthly)
-    # if sum_assets == 1:
-    #     list_purchase_price_monthly = creating_list_purchase_price_montly()
-    #     list_purchase_price_once = creating_list_purchase_price_once(list_purchase_price_monthly)
-    #     print(list_purchase_price_monthly)
-    # else:
-    #     print("Сумма актива не равна 1!")
+def calculate_purchase_prices(asset_proportions: dict, cash: int) -> dict:
+    purchase_prices = {}
+    for asset_name, proportion in asset_proportions.items():
+        purchase_price = proportion * cash
+        purchase_prices[asset_name] = purchase_price
+    return purchase_prices
 
 
-final_calculations()
+def main():
+    portfolio = Portfolio()
+    check_result = check_proportions_assets(portfolio.asset_proportions)
+    if not check_result:
+        print("Условия проверки суммы активов не пройдены")
+        sys.exit()
+
+    purchase_prices = calculate_purchase_prices(portfolio.asset_proportions, portfolio.cash)
+    print(purchase_prices)
+
+
+if __name__ == "__main__":
+    main()
