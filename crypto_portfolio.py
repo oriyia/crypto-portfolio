f3150a14d2c86b2ddf4238fccc57ad2e01f9bba8
Ilya Yarusov <originalyia@gmail.com>
2025-02-04 20:57:11 +0800

    add new share tokens, buys

--- crypto_portfolio.py
+++ crypto_portfolio.py
@@ -1,40 +1,52 @@
 #!/home/oriyia/.virtualenvs/crypto_portfolio/bin/python3
-import sys
-import math
 import json
+import math
+import sys
 from datetime import date
+import math
 
-from portfolio_value import calculate_value_assets, calculate_total_value_portfolio, get_price_coin
 from deposits import DEPOSITS
+from portfolio_value import (calculate_total_value_portfolio,
+                             calculate_value_assets, get_price_coin)
 
 
-class Portfolio():
+class Portfolio:
     def __init__(self):
         self.asset_proportions = {
-                'BTC': 0.25,
-                'BNB': 0.08,
-                'ETH': 0.18,
-                'DOT': 0.05,
-                'NEAR': 0.05,
-                'ADA': 0.05,
-                'ATOM': 0.05,
-                'MATIC': 0.1,
-                'GRT': 0.10,
-                'AGIX': 0.09
+            "BTC": 0.10,  # 0.25
+            "BNB": 0.05,
+            "ETH": 0.1,  # 0.18
+            "DOT": 0.05,
+            "NEAR": 0.05,
+            "ADA": 0.05,
+            "ATOM": 0.05,
+            "MATIC": 0.08,  # 0.1
+            "GRT": 0.04,
+            "AGIX": 0.03,
+            "TON": 0.03,
+            "SNX": 0.04,
+            "ROSE": 0.03,
+            "IMX": 0.04,
+            "INJ": 0.03,
+            "FTM": 0.03,
+            "TIA": 0.08,
+            "SOL": 0.08,
+            "AVAX": 0.04
         }
         self.cash = 120
+        self.cash_rub = 15000
         self.number_averages = 1
         self.number_coins = {
-            'bitcoin': 0.03538349,
-            'ethereum': 0.4567294,
-            'cardano': 205.4967,
-            'cosmos': 28.0603,
-            'binancecoin': 0.819723,
-            'matic-network': 190.13367015,
-            'polkadot': 43.83306,
-            'near': 129.46945,
-            'aave': 0,
-            'acala': 32.168,
+            "bitcoin": 0.03538349,
+            "ethereum": 0.4567294,
+            "cardano": 205.4967,
+            "cosmos": 28.0603,
+            "binancecoin": 0.819723,
+            "matic-network": 190.13367015,
+            "polkadot": 43.83306,
+            "near": 129.46945,
+            "aave": 0,
+            "acala": 32.168,
         }
 
 
@@ -50,8 +62,10 @@ def check_proportions_assets(asset_proportions: dict) -> bool:
 
 def calculate_purchase_prices(asset_proportions: dict, cash: int) -> dict:
     purchase_prices = {}
+    dollar_rate = get_price_coin("tether", "rub")
+    cash_dollar = math.floor(cash / dollar_rate["tether"]["rub"])
     for asset_name, proportion in asset_proportions.items():
-        purchase_price = proportion * cash
+        purchase_price = proportion * cash_dollar
         purchase_prices[asset_name] = purchase_price
     return purchase_prices
 
@@ -81,7 +95,9 @@ def main():
         print("Условия проверки суммы активов не пройдены")
         sys.exit()
 
-    purchase_prices = calculate_purchase_prices(portfolio.asset_proportions, portfolio.cash)
+    purchase_prices = calculate_purchase_prices(
+        portfolio.asset_proportions, portfolio.cash_rub
+    )
     print(purchase_prices)
 
     value_assets = calculate_value_assets(portfolio.number_coins)
@@ -89,9 +105,9 @@ def main():
     total_value_portfolio = calculate_total_value_portfolio(value_assets)
     print(f"Общая стоимость активов: ${total_value_portfolio}")
 
-    dollar_rate = get_price_coin('tether', 'rub')
+    dollar_rate = get_price_coin("tether", "rub")
     print(dollar_rate)
-    value_assets_rub = dollar_rate['tether']['rub'] * total_value_portfolio
+    value_assets_rub = dollar_rate["tether"]["rub"] * total_value_portfolio
     print(f"Рублей: {value_assets_rub}")
 
     total_deposits = calculate_total_value_portfolio(DEPOSITS)
@@ -104,4 +120,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    sums = 0
+    for value in DEPOSITS.values():
+        if isinstance(value, list):
+            sums += sum(value)
+        else:
+            sums += value
+    print(f"Value: {sums}")
